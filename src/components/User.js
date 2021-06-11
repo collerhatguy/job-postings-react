696e528d9ac76a919a548aa709f4074a96ad2556
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-11 18:34:24 -0400

    did component

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,26 @@ import React from 'react'
 export default function User({user}) {
     return (
         <div className="user">
-            
+            <h3 
+                className="user-company">
+                {user.company}
+            </h3>
+            {user.new ? <span>New</span> : null}
+            {user.featured ? <span>Featured</span> : null}
+            <h4 
+                className="user-position">
+                {user.position}
+            </h4>
+            <p>{user.postedAt} * {user.contract} * {user.location}</p>
+            <hr />
+            <ul className="user-language-list">
+                {user.languages.map(language => {
+                    return <li className="user-language">{language}</li>
+                })}
+                {user.tools.map(tool => {
+                    return <li className="user-tool">{tool}</li>
+                })}
+            </ul>
         </div>
     )
 }
