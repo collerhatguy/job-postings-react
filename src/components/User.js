a99672ea83eb72ebf058f3736e1453e78b38ac01
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-12 08:50:31 -0400

    added responsiveness

--- src/components/User.js
+++ src/components/User.js
@@ -4,23 +4,25 @@ export default function User({user}) {
     const url = `.${user.logo}`;
     return (
         <div className="user">
-            <img src={url} />
-            <div className="user-top">
-                <h3 
-                    className="user-company">
-                    {user.company}
-                </h3>
-                {user.new ? <span   className="new">NEW!</span> : null}
-                {user.featured ? <span className="featured">Featured</span> : null}
+            <div className="user-left">
+                <img src={url} />
+                <div className="user-top">
+                    <h3 
+                        className="user-company">
+                        {user.company}
+                    </h3>
+                    {user.new ? <span   className="new">NEW!</span> : null}
+                    {user.featured ? <span className="featured">Featured</span> : null}
+                </div>
+                <h4
+                    className="user-position">
+                    {user.position}
+                </h4>
+                <p>
+                    {user.postedAt} * {user.contract} * {user.location}
+                </p>
             </div>
-            <h4 
-                className="user-position">
-                {user.position}
-            </h4>
-            <p>
-                {user.postedAt} * {user.contract} * {user.location}
-            </p>
-            <hr />
+            <hr/>
             <ul className="user-language-list">
                 {user.languages.map(language => {
                     return <li className="user-language">{language}</li>
