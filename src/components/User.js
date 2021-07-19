699e43b93f93b5d6a9fdace6b29e75f97c78dd29
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-07-19 16:05:45 -0400

    added url code but it didnt work

--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-export default function User({user}) {
-    const url = `/public${user.logo.substring(1, user.logo.length)}`
+export default function User({ user }) {
+    const url = `${process.env.PUBLIC_URL}${user.logo.substring(1, user.logo.length)}`;
+    console.log(url)
     return (
         <div className="user">
             <div className="user-left">
-                <img href={url} alt="logo"/>
+                <img src={user.logo} alt="logo"/>
                 <div className="user-top">
                     <h3 
                         className="user-company">
                         {user.company}
                     </h3>
-                    {user.new ? <span   className="new">NEW!</span> : null}
+                    {user.new ? <span className="new">NEW!</span> : null}
                     {user.featured ? <span className="featured">Featured</span> : null}
                 </div>
                 <h4
@@ -24,12 +25,12 @@ export default function User({user}) {
             </div>
             <hr/>
             <ul className="user-language-list">
-                {user.languages?.map(language => {
-                    return <li className="user-language">{language}</li>
-                })}
-                {user.tools?.map(tool => {
-                    return <li className="user-tool">{tool}</li>
-                })}
+                {user.languages?.map(language => 
+                    <li className="user-language">{language}</li>
+                )}
+                {user.tools?.map(tool => 
+                    <li className="user-tool">{tool}</li>
+                )}
             </ul>
         </div>
     )
